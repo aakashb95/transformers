6de18721b3f8d6424c51126a6e24db93dc1cf2e0
Aakash Bakhle <aakashb@cdac.in>
2022-03-31 12:25:25 +0530

    sort imports

--- src/transformers/models/luke/configuration_luke.py
+++ src/transformers/models/luke/configuration_luke.py
@@ -17,11 +17,12 @@
 from collections import OrderedDict
 from typing import Any, Mapping, Optional
 
+from ... import PreTrainedTokenizerBase, TensorType
 from ...configuration_utils import PretrainedConfig
 from ...onnx import OnnxConfig
-from ...utils import logging
-from ... import PreTrainedTokenizerBase, TensorType
 from ...onnx.utils import compute_effective_axis_dimension
+from ...utils import logging
+
 
 logger = logging.get_logger(__name__)
 
